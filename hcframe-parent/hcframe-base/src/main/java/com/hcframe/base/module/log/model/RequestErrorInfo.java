f43cb56dacd66cfdd3e360081f5ae8ae36d8dc0b
lhc <taixingyiji@126.com>
2021-02-23 09:25:10 +0800

    修复activiti，新增日志上传

---
+++ hcframe-parent/hcframe-base/src/main/java/com/hcframe/base/module/log/model/RequestErrorInfo.java
@@ -0,0 +1,19 @@
+package com.hcframe.base.module.log.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author lhc
+ */
+@Data
+public class RequestErrorInfo implements Serializable {
+    private static final long serialVersionUID = 2671491007419047891L;
+    private String ip;
+    private String url;
+    private String httpMethod;
+    private String classMethod;
+    private Object requestParams;
+    private RuntimeException exception;
+}
---
+++ hcframe-parent/hcframe-base/src/main/java/com/hcframe/base/module/log/model/RequestInfo.java
@@ -0,0 +1,22 @@
+package com.hcframe.base.module.log.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+/**
+ * @author lhc
+ */
+@Data
+public class RequestInfo implements Serializable {
+    private static final long serialVersionUID = 386274939008099288L;
+    private String ip;
+    private String url;
+    private String httpMethod;
+    private String classMethod;
+    private Object requestParams;
+    private Object result;
+    private Long timeCost;
+
+}
