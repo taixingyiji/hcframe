bc6411805944d75d9d718a5433974159a1210bde
wcheng <jiajia_2311@163.com>
2021-04-28 11:31:01 +0800

    add loganno

--- hcframe-parent/hcframe-user/src/main/java/com/hcframe/user/module/manage/controller/ManageGwController.java
+++ hcframe-parent/hcframe-user/src/main/java/com/hcframe/user/module/manage/controller/ManageGwController.java
@@ -3,6 +3,7 @@ package com.hcframe.user.module.manage.controller;
 import com.github.pagehelper.PageInfo;
 import com.hcframe.base.common.ResultVO;
 import com.hcframe.base.common.WebPageInfo;
+import com.hcframe.base.module.log.annotation.LogAnno;
 import com.hcframe.user.module.manage.service.ManageGwService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,18 +25,21 @@ public class ManageGwController {
 	@Autowired ManageGwService service;
 
 	@PostMapping()
+	@LogAnno(operateType="新增馆外用户",moduleName="系统管理-用户管理-馆外用户信息管理-新增馆外用户")
     @ApiOperation(value = "新增用户信息", notes = "将自动传承ey-value对象模式即可")
     public ResultVO<Map<String,Object>> addUser(@RequestParam Map<String,Object> user) {
         return service.addUser(user);
     }
 
 	@PutMapping("/{version}")
+	@LogAnno(operateType="编辑馆外用户信息",moduleName="系统管理-用户管理-馆外用户信息管理-编辑馆外用户信息")
     @ApiOperation(value = "更新用户信息")
     public ResultVO<Integer> updateUser(@RequestParam Map<String,Object> user,@PathVariable Integer version) {
         return service.updateUser(user,version);
     }
 
     @DeleteMapping("/{ids}")
+    @LogAnno(operateType="删除馆外用户",moduleName="系统管理-用户管理-馆外用户信息管理-删除馆外用户")
     @ApiOperation(value = "删除用户（逻辑删除）", notes = "删除后职位也会被删除")
     public ResultVO<Integer> deleteUser(@PathVariable String ids) {
         return service.deleteUser(ids);
@@ -48,13 +52,15 @@ public class ManageGwController {
     }
 
     @PutMapping("disable/{version}")
-    @ApiOperation(value = "启用/禁用",notes = "用户启用禁用")
+    @LogAnno(operateType="启用/禁用馆外用户",moduleName="系统管理-用户管理-馆外用户信息管理-启用/禁用馆外用户")
+    @ApiOperation(value = "启用/禁用馆外用户",notes = "用户启用禁用")
     public ResultVO<Integer> disable(Boolean enabled,String userId,@PathVariable Integer version) {
         return service.disable(enabled,userId,version);
     }
 
     @PutMapping("/resetPassword/{version}")
-    @ApiOperation(value = "重置密码",notes = "用户启用禁用")
+    @LogAnno(operateType="馆外用户重置密码",moduleName="系统管理-用户管理-馆外用户信息管理-馆外用户重置密码")
+    @ApiOperation(value = "馆外用户重置密码",notes = "用户启用禁用")
     public ResultVO<Integer> resetPassword(String userId,@PathVariable Integer version) {
         return service.resetPassword(userId,version);
     }
